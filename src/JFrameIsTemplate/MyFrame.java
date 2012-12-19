3bfcc78a4f47c63a1bee241ddb1b22cb46dbf890
Tom Cowling <tom.cowling@gmail.com>
2012-12-19 15:12:09 +0000

    Added a couple of template method examples

---
+++ src/JFrameIsTemplate/MyFrame.java
@@ -0,0 +1,23 @@
+package JFrameIsTemplate;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame{
+    public MyFrame(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300,300);
+        this.setVisible(true);
+    }
+
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        String msg = "Is this a message?";
+        graphics.drawString(msg, 100, 100);
+    }
+
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame("Head First Design Patterns");
+    }
+}
---
+++ src/SortingWithTemplateMethod/DuckSortTestDrive.java
@@ -0,0 +1,29 @@
+package SortingWithTemplateMethod;
+
+import java.util.Arrays;
+
+public class DuckSortTestDrive {
+    public static void main(String[] args) {
+        Duck[] ducks = {
+                new Duck("Daffy", 8),
+                new Duck("Dewey", 2),
+                new Duck("Howard", 7),
+                new Duck("Howard", 2),
+                new Duck("Howard", 10),
+                new Duck("Howard", 2)
+        };
+
+        System.out.println("Before sorting:");
+        display(ducks);
+        Arrays.sort(ducks);
+
+        System.out.println("\nAfter sorting");
+        display(ducks);
+    }
+
+    public static void display(Duck[] ducks) {
+        for (Duck duck : ducks) {
+            System.out.println(duck);
+        }
+    }
+}
